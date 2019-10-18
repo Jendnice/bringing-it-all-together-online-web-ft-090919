5e20c63dc48d4c35bcc7306ea66f84bbcc68a897
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 16:50:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -95,19 +95,36 @@ class Dog
   #   song
   # end
   
-  def self.find_or_create_by(name:, breed:)
-   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-   if !dog.empty?
-     dog_data = dog[0]
-     dog = Dog.new(dog_data)
-   else 
-     attributes = {
-      :name => name,
-      :breed => breed
-      }
-     dog = self.create(attributes)
-   end 
-   dog 
-  end 
+  # def self.find_or_create_by(name:, breed:)
+  # dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+  # if !dog.empty?
+  #   dog_data = dog[0]
+  #   dog = Dog.new(dog_data)
+  # else 
+  #   attributes = {
+  #     :name => name,
+  #     :breed => breed
+  #     }
+  #   dog = self.create(attributes)
+  # end 
+  # dog 
+  # end 
+  
+   def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+
+
+      dog = DB[:conn].execute(sql, name, breed).first
+
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
+  end
   
 end 

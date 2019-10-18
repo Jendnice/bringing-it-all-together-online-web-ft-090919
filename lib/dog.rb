f81c30a0439082f537174935cfd92e1421f0e06f
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 16:02:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,7 @@ class Dog
   
   def self.create_table
     sql = <<-SQL
-    INSERT TABLE dog (
+    INSERT TABLE IF NOT EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT

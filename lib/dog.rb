0b0784040221857d600d40b104b7c6b36973d999
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 16:10:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ class Dog
       SQL
       
       DB[:conn].execute(sql, self.name self.breed)
-      @id = DB[:conn].execute()
+      @id = DB[:conn].execute("SELECT last insert rowid")
     
   end 
   

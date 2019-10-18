30a5c96fcf4007249c031e20bcc6ce4741c83333
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 16:42:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,9 +84,22 @@ class Dog
     Dog.new_from_db(result)
   end 
   
-  def self.find_or_create_by(name: , breed:)
-   if !self.name.empty?
-     self.find_by_name(name)
+  def self.find_or_create_by(name:, album:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+   if !dog.empty?
+     dog_data = dog[0]
+     dog = Dog.new(dog_data)
    else 
      attributes = (name: , breed:)
      self.create(attributes)

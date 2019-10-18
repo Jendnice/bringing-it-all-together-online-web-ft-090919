cd40a68b8862f12321bb92c25f481251269e0b93
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 16:00:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,16 +7,16 @@ class Dog
     self.id ||= nil 
   end 
   
-  def self.create_table
-    sql = <<-SQL
-     INSERT TABLE dogs(
-       id INTEGER PRIMARY KEY,
-       name TEXT,
-       breed TEXT
-     );
-    SQL
+  # def self.create_table
+  #   sql = <<-SQL
+  #   INSERT TABLE dogs(
+  #     id INTEGER PRIMARY KEY,
+  #     name TEXT,
+  #     breed TEXT
+  #   );
+  #   SQL
     
-    DB[:conn].execute(sql)
-  end 
+  #   DB[:conn].execute(sql)
+  # end 
   
 end 

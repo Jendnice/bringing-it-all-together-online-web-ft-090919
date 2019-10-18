8ee4d74ecc6470741bcaeacfbc32ef58a828b9e3
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 15:53:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,12 @@ class Dog
   attr_accessor :id, :name, :breed 
   
   def attributes(id: nil, name: , breed: )
-    params = {id: id, name: name, breed: breed}
-     def initialize(params)
-     @id = id 
-     @name = name
-     @breed = breed 
-     end 
+    # params = {id: id, name: name, breed: breed}
+    # def initialize(params)
+    # @id = id 
+    # @name = name
+    # @breed = breed 
+    # end 
   end 
   
 end 
